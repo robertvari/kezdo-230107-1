af319617dfbca10f079b39c7ef2cb8fe127a7e4d
Robert Vari <mail.robertvari@gmail.com>
2023-01-07 15:33:05 +0100

    update

---
+++ say_my_name.py
@@ -0,0 +1,7 @@
+name = input("Your name?")
+age = input("Your age?")
+email = input("Your email?")
+
+print(f"Hello {name}!\nI'm an AI.\nHow are you?")
+print(f"You are {age} old?? You are older than me! :)")
+print(f"Thank you for your email: {email}. I'll write something funny to you. :)")
