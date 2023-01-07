992f7fc1379d0fa11903112575398995d0d4ef58
Robert Vari <mail.robertvari@gmail.com>
2023-01-07 17:09:04 +0100

    update

--- 11_dictionary.py
+++ 11_dictionary.py
@@ -5,4 +5,21 @@ car_registry = {
     "NGD-765": {"name":"Csilla", "address": "Debrecen", "phone":"06 30 435 7874"},
 }
 
+# get data from dictionary
 print(car_registry["FGR-543"]["address"])
+
+# get keys or values from a dict
+print(car_registry.keys())
+print(car_registry.values())
+
+# add new item to a dictionary
+car_registry["HGF-936"] = {"name":"Laci", "address": "Pécs", "phone":"06 20 484 6254"}
+
+# delete item from dictionary
+del car_registry["HGF-936"]
+
+# update item in dictionary
+car_registry["ABC-123"] = {"name":"Géza", "address": "Debrecen", "phone":"06 30 999 6754"}
+car_registry["ABC-123"]["address"] = "Bécs"
+
+print(car_registry["ABC-123"])
