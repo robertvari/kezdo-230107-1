7bdcd32078b109d398729475ae31e52c44bef7e8
Robert Vari <mail.robertvari@gmail.com>
2023-01-07 18:08:09 +0100

    update

--- 16_if_and.py
+++ 16_if_and.py
@@ -1,10 +1,14 @@
+import getpass
+
 username = "robert"
 password = "testpas123"
 
 username_input = input("Username?")
-password_input = input("Password?")
+password_input = getpass.getpass("Password?")
 
-if username == username_input and password == password_input:
+print("User password:", password_input)
+#            True                             True
+if (username == username_input) and (password == password_input):
     print(f"Hello {username}. How are you today?")
 else:
     print("username or password is incorrect. Try again.")
