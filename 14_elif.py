d75c7fe301661c020c628ef5cc4b22170a4ec655
Robert Vari <mail.robertvari@gmail.com>
2023-01-07 17:37:37 +0100

    update

---
+++ 14_elif.py
@@ -0,0 +1,15 @@
+status = 501
+
+
+if status == 100:
+    print("Status: Continue")
+elif status == 200:
+    print("Status: OK")
+elif status == 300:
+    print("Status: Multiple Choices")
+elif status == 400:
+    print("Status: Bad Request")
+elif status == 500:
+    print("Status: Internal Server Error")
+else:
+    print("This status is not handled :(")
