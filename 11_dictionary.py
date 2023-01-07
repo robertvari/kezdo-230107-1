78f56642f6cc1e1ee73ef85573f8ef24b662de16
Robert Vari <mail.robertvari@gmail.com>
2023-01-07 16:55:15 +0100

    update

---
+++ 11_dictionary.py
@@ -0,0 +1,8 @@
+car_registry = {
+    "ABC-123": {"name":"Kriszta", "address": "Pécs", "phone":"06 20 543 6754"},
+    "FGR-543": {"name":"Csaba", "address": "Budapest", "phone":"06 20 123 7568"},
+    "RGE-585": {"name":"Tamás", "address": "Miskolc", "phone":"06 30 876 9258"},
+    "NGD-765": {"name":"Csilla", "address": "Debrecen", "phone":"06 30 435 7874"},
+}
+
+print(car_registry["FGR-543"]["address"])
