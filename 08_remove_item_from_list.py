95c3265a553e3306434a96afc7b879eedf552f20
Robert Vari <mail.robertvari@gmail.com>
2023-01-07 15:58:06 +0100

    update

---
+++ 08_remove_item_from_list.py
@@ -0,0 +1,11 @@
+my_friends = ["Csaba", "Kriszta", "Tamás", "Csilla"]
+
+# remove item from list
+my_friends.remove("Tamás")
+
+print(my_friends)
+
+# delete last item from list
+del my_friends[-1]
+
+print(my_friends)
