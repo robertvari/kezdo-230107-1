ef2de4dc910291b6f45a6ed566f73a5c14e89592
Robert Vari <mail.robertvari@gmail.com>
2023-01-08 14:53:51 +0100

    update

---
+++ 21_slicing.py
@@ -0,0 +1,16 @@
+my_friends = [
+    "kriszta", 
+    "csaba", 
+    "csilla", 
+    "zoli", 
+    "Betty", 
+    "Nicole", 
+    "Sapphire", 
+    "Madiha"
+]
+
+# my_list[START_INDEX : STOP_INDEX : STEP]
+print(my_friends[:3])
+
+for name in my_friends[3:]:
+    print(name)
