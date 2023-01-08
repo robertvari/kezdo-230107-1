f4311eefcc563195fa4f133cc19d7c48f3033df2
Robert Vari <mail.robertvari@gmail.com>
2023-01-08 14:59:14 +0100

    update

--- 20_for_break.py
+++ 20_for_break.py
@@ -1,7 +1,7 @@
 my_friends = ["kriszta", "csaba", "csilla", "zoli"]
 
 for name in my_friends:
+    print(name)
+
     if name == "csilla": # exit loop on this item
         break
-
-    print(name)
