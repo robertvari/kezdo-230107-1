d4e5fcc36ede9dcdf3776024ec38528a9a24fed0
Robert Vari <mail.robertvari@gmail.com>
2023-01-08 15:38:27 +0100

    update

--- magic_number.py
+++ magic_number.py
@@ -1,10 +1,16 @@
 print("="*50, "MAGIC NUMBER", "="*50)
 print("I have a number between 1 and 10. Can you guess it?")
 
+lifes = 3
 magic_number = 5
 player_guess = input("Your number?")
 
 while player_guess != str(magic_number):
+    lifes -= 1
+    if lifes == 0:
+        print("You lost this game. Maybe next time :(")
+        exit()
+
     print("Wrong guess :( Try again")
     player_guess = input("Your number?")
 
