b3ac6c460bb53d77869c392940a316b23f29bd7e
Robert Vari <mail.robertvari@gmail.com>
2023-01-08 15:23:39 +0100

    update

---
+++ magic_number.py
@@ -0,0 +1,7 @@
+print("="*50, "MAGIC NUMBER", "="*50)
+print("I have a number between 1 and 10. Can you guess it?")
+
+magic_number = 5
+player_guess = input("Your number?")
+
+print(player_guess, magic_number, player_guess == str(magic_number))
