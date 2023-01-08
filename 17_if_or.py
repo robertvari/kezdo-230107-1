bcbfb34b367c67529d1ef4949e1f6dc112cecf95
Robert Vari <mail.robertvari@gmail.com>
2023-01-08 14:10:17 +0100

    update

---
+++ 17_if_or.py
@@ -0,0 +1,7 @@
+today = "Saturday"
+
+#         True                     False
+if (today == "Sunday") or (today == "Saturday"):
+    print("Python Suli!")
+else:
+    print("This is a weekday. No Python Suli")
