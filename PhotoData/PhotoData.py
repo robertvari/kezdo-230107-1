5537248df39476aec43d067d25e3d73912702440
Robert Vari <mail.robertvari@gmail.com>
2023-01-08 17:07:41 +0100

    update

--- PhotoData/PhotoData.py
+++ PhotoData/PhotoData.py
@@ -1,7 +1,7 @@
 import os
 
 # r = raw string
-folder_path = r"D:\Work\_PythonSuli\kezdo-230107\photos\IMG_1070.JPG"
+folder_path = r"D:\Work\_PythonSuli\kezdo-230107\photos"
 
 # check if path exists
 assert os.path.exists(folder_path), f"The path: {folder_path} doesn't exist :((("
@@ -10,7 +10,20 @@ assert os.path.exists(folder_path), f"The path: {folder_path} doesn't exist :(((
 assert os.path.isdir(folder_path), f"The path: {folder_path} must be a directory. :((("
 
 # load files and folders from folder_path
-files = os.listdir(folder_path)
+folder_content = os.listdir(folder_path)
 
-for i in files:
-    print(i)
+# todo: filter folder content. Only .jpg files allowed!
+alowed_extensions = ["jpg", "jpeg"]
+
+for item in folder_content:
+    full_path = os.path.join(folder_path, item)
+    
+    # todo skip folders
+    if os.path.isdir(full_path):
+        continue
+
+    item_list = item.lower().split(".")
+    extension = item_list[-1]
+
+    if extension in alowed_extensions:
+        print(full_path)
